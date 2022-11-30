9874fe4e6467b041ddb1c00584245e2da2b66d40
Ageng Wibowo <ageng.wibowo@dataon.com>
2022-12-01 00:10:45 +0700

    update routing

--- src/Routing.js
+++ src/Routing.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './styles/index.css';
 import Login from './pages/Login';
 import Product from './pages/Product';
+import { GlobalContext } from './context/GlobalContext';
 
 export default function App() {
+  const { userCookies } = useContext(GlobalContext);
   return (
     <>
       <Route path="/login">
@@ -13,6 +15,9 @@ export default function App() {
       </Route>
       <Route path="/product">
         <Product />
+      </Route>
+      <Route path="/">
+        <Redirect to={userCookies ? '/product' : '/login'} />
       </Route>
     </>
   );
