a2d96abdbf24e64f7252963b0936a1a94e3cd399
Ageng Wibowo <ageng.wibowo@dataon.com>
2022-11-30 23:47:58 +0700

    implement search product

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,45 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { Space, Table } from 'antd';
+import { Col, Input, Row, Space, Table } from 'antd';
 import Detail from '../../components/product/detail';
 import axios from 'axios';
 
 const { Column } = Table;
-const data = [
-  {
-    key: '1',
-    id: '1',
-    product_name: 'Product 1',
-    status_product: 'a'
-  },
-  {
-    key: '2',
-    id: '2',
-    product_name: 'Product 2',
-    status_product: 'b'
-  }
-];
+const { Search } = Input;
+
 const Product = () => {
-  //   const [productData, setProductData] = useState();
+  const [productData, setProductData] = useState([]);
+  const [isProductDataLoading, setIsProductLoading] = useState();
   const [isModalDetailVisible, setIsModalDetailVisible] = useState(false);
   const [modalDetailType, setModalDetailType] = useState();
   const [selectedProductData, setSelectedProductData] = useState();
 
   const productUrl = 'https://tangy-worlds-drive-103-95-41-46.loca.lt/products';
 
-
-const getProduct = () => {
+  const getProduct = (product_name = '') => {
+    setIsProductLoading(true);
     axios
       .get(productUrl, {
         params: {
           page: 1,
           limit: 10,
           sort: 'product_name',
-          order: 'asc'
+          order: 'asc',
+          product_name
         },
         headers: { 'Access-Control-Allow-Origin': '*' },
         mode: 'no-cors'
       })
-      .then((res) => console.log(res));
+      .then((res) => {
+        const { data } = res;
+        if (data.length) {
+          setProductData(data);
+        } else {
+          setProductData([]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsProductLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -57,6 +60,10 @@ const getProduct = () => {
     setIsModalDetailVisible(false);
   };
 
+  const onSearch = (value) => {
+    getProduct(value);
+  };
+
   return (
     <>
       <Detail
@@ -65,9 +72,22 @@ const getProduct = () => {
         data={selectedProductData}
         type={modalDetailType}
       />
-      <Table dataSource={data}>
+      <Row>
+        <Col>
+          <Search
+            placeholder="input search text"
+            allowClear
+            onSearch={onSearch}
+            style={{
+              width: 200,
+              marginBottom: 8
+            }}
+          />
+        </Col>
+      </Row>
+      <Table dataSource={productData} loading={isProductDataLoading}>
         <Column title="Product Name" dataIndex="product_name" key="product_name" />
-        <Column title="Status Product" dataIndex="status_product" key="status_product" />
+        <Column title="Memory" dataIndex="memory" key="memory" />
         <Column
           title="Action"
           key="action"
